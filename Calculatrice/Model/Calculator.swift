8b95ee9d876f244169cc43f162e78a92283e4f30
Rochdi Benabdeljelil <rochdi92@hotmail.com>
2020-02-20 14:22:28 +0300

    modification du code

--- Calculatrice/Model/Calculator.swift
+++ Calculatrice/Model/Calculator.swift
@@ -7,3 +7,130 @@
 //
 
 import Foundation
+class Calculator {
+    var numberOnScreen = "" {
+        didSet {
+            NotificationCenter.default.post(Notification(name: Notification.Name("updateCalcul")))
+        }
+    }
+     var elements: [String] {
+        return  numberOnScreen.split(separator: " ").map { "\($0)" }
+    }
+    private var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
+    }
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
+    }
+    var expressionHaveResult: Bool {
+        return numberOnScreen.firstIndex(of: "=") != nil
+    }
+    var divideByZero: Bool {
+        return numberOnScreen.contains("/ 0")
+    }
+    func addNumber(_ number: String) {
+        if expressionHaveResult {
+            numberOnScreen = ""
+        }
+        numberOnScreen.append(number)
+    }
+    func result() {
+        if expressionHaveResult {
+            if let result = elements.last {
+                numberOnScreen = result
+            }
+        }
+    }
+    func dotPressed( left: Double, right: Double ) {
+        numberOnScreen = String(left + right)
+    }
+    func clearNumber() {
+        numberOnScreen = ""
+    }
+    func addOperations(_ element: String) {
+        result()
+        if canAddOperator {
+            switch element {
+            case " + ":
+                numberOnScreen.append(" + ")
+            case " - ":
+                numberOnScreen.append(" - ")
+            case " * ":
+                numberOnScreen.append(" x ")
+            case " / ":
+                numberOnScreen.append(" / ")
+            case ".":
+                numberOnScreen.append(" . ")
+            default:
+                break
+            }
+        } else {
+            NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                         userInfo: ["message": "Un operateur est déja mis !"]))
+        }
+    }
+    func format(_ number: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.minimumIntegerDigits = 1
+        formatter.maximumFractionDigits = 8
+        guard let stringNumber = formatter.string(from: NSNumber(value: number)) else { return ""}
+        return stringNumber
+    }
+    // Create local copy of operations
+    func calcul(left: Double, right: Double, operand: String) -> Double {
+        var result: Double = 0
+        switch operand {
+        case "+": result = left + right
+        case "-": result = left - right
+        case "x": result = left * right
+        case "/": result = left / right
+        default: break
+        }
+        return result
+    }
+    func equal() {
+        guard !expressionHaveResult else {
+            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                userInfo: ["message": "Entrez une nouvelle opération"]))
+        }
+        guard expressionIsCorrect else {
+            numberOnScreen = ""
+            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                            userInfo: ["message": "Entrez une expression correcte"]))
+        }
+        guard expressionHaveEnoughElement else {
+            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                userInfo: ["message": "Commencez un nouveau calcul"]))
+        }
+        guard divideByZero == false else {
+            numberOnScreen = ""
+            return  NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                 userInfo: ["message": "Division par 0 impossible"]))
+        }
+        // Create local copy of operations
+        var operationsToReduce = elements
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            var left = Double(operationsToReduce[0])!
+            var operand = operationsToReduce[1]
+            var right = Double(operationsToReduce[2])!
+            var operandIndex = 1 // because no sign will be at base index 0 otherwise error
+            let result: Double
+            if let index = operationsToReduce.firstIndex(where: {["x", "/"].contains($0)}) {
+                operandIndex = index
+                left = Double(operationsToReduce[index - 1])!
+                operand = operationsToReduce[index]
+                right = Double(operationsToReduce[index + 1])!
+            }
+            result = calcul(left: left, right: right, operand: operand)
+            for _ in 1...3 {
+                operationsToReduce.remove(at: operandIndex - 1)
+            }
+            operationsToReduce.insert(format(result), at: operandIndex - 1 )
+        }
+        numberOnScreen.append(" = \(operationsToReduce.first!)")
+    }
+}
--- Calculatrice/ViewController.swift
+++ Calculatrice/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
+    var calcul = Calculator()
+    // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(updateText), name: Notification.Name("updateCalcul"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(displayError(_:)), name: Notification.Name("error"), object: nil)
         // Do any additional setup after loading the view.
     }
-
-
+    // View actions
+    @IBAction func tappedNumberButton(_ sender: UIButton) {
+        guard let numberText = sender.title(for: .normal) else {
+            return
+        }
+        calcul.addNumber(numberText)
+    }
+    @IBAction func tappedAdditionButton(_ sender: UIButton) {
+        calcul.addOperations(" + ")
+    }
+    @IBAction func tappedSubstractionButton(_ sender: UIButton) {
+        calcul.addOperations(" - ")
+    }
+    @IBAction func tappedDivideButton(_ sender: UIButton) {
+        calcul.addOperations(" / ")
+    }
+    @IBAction func tappedMultiplyButton(_ sender: UIButton) {
+        calcul.addOperations(" * ")
+    }
+    @IBAction func tappedDotButton(_ sender: UIButton) {
+        calcul.addNumber(" . ")
+    }
+    @IBAction func tappedEqualButton(_ sender: UIButton) {
+        calcul.equal()
+    }
+    @IBAction func tappedClearButton(_ sender: UIButton) {
+        calcul.clearNumber()
+    }
+    @objc func updateText() {
+        textView.text = calcul.numberOnScreen
+    }
+    // same alerte with title and message as parameter
+       func alert(_ message: String) {
+           let alertVC = UIAlertController(title: "Erreur", message: message, preferredStyle: .alert)
+           alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+           present(alertVC, animated: true, completion: nil)
+       }
+    @objc func displayError(_ notif: Notification) {
+        if let message = notif.userInfo?["message"] as? String {
+            alert(message)
+        } else {
+            alert("Erreur Inconnue")
+        }
+    }
 }
-
