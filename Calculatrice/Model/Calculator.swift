f6ab3e55fbbc5f14e10013200c2c91a0b85f85b9
Rochdi Benabdeljelil <rochdi92@hotmail.com>
2020-02-20 14:20:23 +0300

    first commit

---
+++ Calculatrice/Model/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  Calculatrice
+//
+//  Created by rochdi ben abdeljelil on 20.02.2020.
+//  Copyright © 2020 rochdi ben abdeljelil. All rights reserved.
+//
+
+import Foundation
