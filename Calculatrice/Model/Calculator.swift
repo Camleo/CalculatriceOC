e46e748f01d226fca4e9823970cf8e6ea2002808
Rochdi Benabdeljelil <rochdi92@hotmail.com>
2020-03-04 23:52:28 +0300

    Test 100%

--- Calculatrice/Model/Calculator.swift
+++ Calculatrice/Model/Calculator.swift
@@ -13,39 +13,38 @@ class Calculator {
             NotificationCenter.default.post(Notification(name: Notification.Name("updateCalcul")))
         }
     }
-    var elements: [String] {
+    private var elements: [String] {
         return  numberOnScreen.split(separator: " ").map { "\($0)" }
     }
-    private var expressionIsCorrect: Bool {
+    var expressionIsCorrect: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
     }
     var expressionHaveEnoughElement: Bool {
         return elements.count >= 3
     }
-    var canAddOperator: Bool {
+    private var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
     }
-    var isLastElementNumber: Bool {
+    private var isLastElementNumber: Bool {
         guard let last = elements.last else{
             return false
         }
         return Double(last) != nil
     }
-    var expressionHaveResult: Bool {
+    private var expressionHaveResult: Bool {
         return numberOnScreen.firstIndex(of: "=") != nil
     }
-    var divideByZero: Bool {
+    private var divideByZero: Bool {
         return numberOnScreen.contains("/ 0")
     }
-    var currentNumberHasDecimal: Bool {
+    private var currentNumberHasDecimal: Bool {
         guard let last = elements.last else{
             return false
         }
         if last.contains(".") {
             return true
-        } else {
-            return false
         }
+        return false
     }
     
     func addNumber(_ number: String) {
@@ -53,121 +52,134 @@ class Calculator {
             numberOnScreen = ""
         }
         if number == "." {
-            if !isLastElementNumber {
-                numberOnScreen.append("0.")
-            }else{
-                if !currentNumberHasDecimal {
+            /* if !isLastElementNumber {
+             numberOnScreen.append("0.")
+             }else{
+             if !currentNumberHasDecimal {
+             numberOnScreen.append(number)
+             
+             }else{
+             NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+             userInfo: ["message": "Le nombre à deja un point !"]))
+             }
+             }
+             }else{
+             numberOnScreen.append(number)
+             }
+             
+             }*/
+            if !currentNumberHasDecimal {
+                if isLastElementNumber {
+                    numberOnScreen.append(".")
+                }else{
                     numberOnScreen.append(number)
-                    
+                }
                 }else{
                     NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                                 userInfo: ["message": "Le nombre à deja un point !"]))
+                                                           userInfo: ["message": "Un operateur est déja mis !"]))
+                }
+            }else{
+                numberOnScreen.append(number)
+            }
+        }
+        func result() {
+            if expressionHaveResult {
+                if let result = elements.last {
+                    numberOnScreen = result
                 }
             }
-        }else{
-            numberOnScreen.append(number)
         }
+        func clearNumber() {
+            numberOnScreen = ""
+        }
+        func addOperations(_ element: String) {
+            result()
+            if canAddOperator && isLastElementNumber {
+                switch element {
+                case "+":
+                    numberOnScreen.append(" + ")
+                case "-":
+                    numberOnScreen.append(" - ")
+                case "*":
+                    numberOnScreen.append(" x ")
+                case "/":
+                    numberOnScreen.append(" / ")
+                default:
+                    break
+                }
+            } else {
+                
+                if  canAddOperator && !isLastElementNumber {
+                    NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                 userInfo: ["message": "Commencez par rentrer un chiffre !"]))
+                }else{
+                    NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                 userInfo: ["message": "Un operateur est déja mis !"]))
+                }
+            }
+        }
+        func format(_ number: Double) -> String {
+            let formatter = NumberFormatter()
+            formatter.minimumIntegerDigits = 1
+            formatter.maximumFractionDigits = 8
+            guard let stringNumber = formatter.string(from: NSNumber(value: number)) else { return ""}
+            return stringNumber
+        }
+        // Create local copy of operations
+        func calcul(left: Double, right: Double, operand: String) -> Double {
+            print(elements)
+            var result: Double = 0
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            case "x": result = left * right
+            case "/": result = left / right
+            default: break
+            }
+            return result
+        }
+        //MARK: - Operations and Equal
         
-    }
-    func result() {
-        if expressionHaveResult {
-            if let result = elements.last {
-                numberOnScreen = result
+        func equal() {
+            guard !expressionHaveResult else {
+                return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                    userInfo: ["message": "Entrez une nouvelle opération"]))
             }
-        }
-    }
-    func clearNumber() {
-        numberOnScreen = ""
-    }
-    func addOperations(_ element: String) {
-        result()
-        if canAddOperator && isLastElementNumber {
-            switch element {
-            case "+":
-                numberOnScreen.append(" + ")
-            case "-":
-                numberOnScreen.append(" - ")
-            case "*":
-                numberOnScreen.append(" x ")
-            case "/":
-                numberOnScreen.append(" / ")
-            case ".":
-                numberOnScreen.append(".")
-            default:
-                break
+            guard expressionIsCorrect else {
+                numberOnScreen = ""
+                return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                    userInfo: ["message": "Entrez une expression correcte"]))
             }
-        } else {
-            
-            if !isLastElementNumber {
-                NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                             userInfo: ["message": "Commencez par rentrer un chiffre !"]))
-            }else{
-                NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                             userInfo: ["message": "Un operateur est déja mis !"]))
+            guard expressionHaveEnoughElement else {
+                return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                    userInfo: ["message": "Commencez un nouveau calcul"]))
             }
-        }
-    }
-    func format(_ number: Double) -> String {
-        let formatter = NumberFormatter()
-        formatter.minimumIntegerDigits = 1
-        formatter.maximumFractionDigits = 8
-        guard let stringNumber = formatter.string(from: NSNumber(value: number)) else { return ""}
-        return stringNumber
-    }
-    // Create local copy of operations
-    func calcul(left: Double, right: Double, operand: String) -> Double {
-        print(elements)
-        var result: Double = 0
-        switch operand {
-        case "+": result = left + right
-        case "-": result = left - right
-        case "x": result = left * right
-        case "/": result = left / right
-        default: break
-        }
-        return result
-    }
-    //MARK: - Operations and Equal
-    
-    func equal() {
-        guard !expressionHaveResult else {
-            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                                userInfo: ["message": "Entrez une nouvelle opération"]))
-        }
-        guard expressionIsCorrect else {
-            numberOnScreen = ""
-            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                                userInfo: ["message": "Entrez une expression correcte"]))
-        }
-        guard expressionHaveEnoughElement else {
-            return NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                                userInfo: ["message": "Commencez un nouveau calcul"]))
-        }
-        guard divideByZero == false else {
-            numberOnScreen = ""
-            return  NotificationCenter.default.post(Notification(name: Notification.Name("error"),
-                                                                 userInfo: ["message": "Division par 0 impossible"]))
-        }
-        var operationsToReduce = elements
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            var left = Double(operationsToReduce[0])!
-            var operand = operationsToReduce[1]
-            var right = Double(operationsToReduce[2])!
-            var operandIndex = 1 // because no sign will be at base index 0 otherwise error
-            let result: Double
-            if let index = operationsToReduce.firstIndex(where: {["x", "/"].contains($0)}) {
-                operandIndex = index
-                left = Double(operationsToReduce[index - 1])!
-                operand = operationsToReduce[index]
-                right = Double(operationsToReduce[index + 1])!
+            guard divideByZero == false else {
+                numberOnScreen = ""
+                return  NotificationCenter.default.post(Notification(name: Notification.Name("error"),
+                                                                     userInfo: ["message": "Division par 0 impossible"]))
             }
-            result = calcul(left: left, right: right, operand: operand)
-            for _ in 1...3 {
-                operationsToReduce.remove(at: operandIndex - 1)
+            var operationsToReduce = elements
+            // Iterate over operations while an operand still here
+            while operationsToReduce.count > 1 {
+                var left = Double(operationsToReduce[0])!
+                var operand = operationsToReduce[1]
+                var right = Double(operationsToReduce[2])!
+                var operandIndex = 1 // because no sign will be at base index 0 otherwise error
+                let result: Double
+                if let index = operationsToReduce.firstIndex(where: {["x", "/"].contains($0)}) {
+                    operandIndex = index
+                    left = Double(operationsToReduce[index - 1])!
+                    operand = operationsToReduce[index]
+                    right = Double(operationsToReduce[index + 1])!
+                }
+                result = calcul(left: left, right: right, operand: operand)
+                for _ in 1...3 {
+                    operationsToReduce.remove(at: operandIndex - 1)
+                }
+                operationsToReduce.insert(format(result), at: operandIndex - 1 )
             }
-            operationsToReduce.insert(format(result), at: operandIndex - 1 )
+            numberOnScreen.append(" = \(operationsToReduce.first!)")
         }
-        numberOnScreen.append(" = \(operationsToReduce.first!)")
-    }
 }
+
--- CalculatriceTests/CalculatriceTests.swift
+++ CalculatriceTests/CalculatriceTests.swift
@@ -10,32 +10,32 @@ import XCTest
 @testable import Calculatrice
 
 class CalculatriceTests: XCTestCase {
-
-  var calculator: Calculator!
+    
+    var calculator: Calculator!
     
     override func setUp() {
         super.setUp()
-       calculator = Calculator()
+        calculator = Calculator()
     }
     
     // MARK: - test unitaire
     
     // test isCorrect
-   func testGivenexpressionHaveEnoughtElement_WhenStringgreaterThanOrEqualTo3_ThenReturnTrue() {
+    func testGivenexpressionHaveEnoughtElement_WhenStringgreaterThanOrEqualTo3_ThenReturnTrue() {
         calculator.numberOnScreen = "2 + 3"
         XCTAssertTrue(calculator.expressionHaveEnoughElement)
     }
-
     
-        func testGivenCalculStringHaveResult_WhenReset_ThenCalculStringIsEmpty(){
+    
+    func testGivenCalculStringHaveResult_WhenReset_ThenCalculStringIsEmpty(){
         calculator.clearNumber()
-               
-               XCTAssertEqual(calculator.numberOnScreen, "")
+        
+        XCTAssertEqual(calculator.numberOnScreen, "")
         
         
     }
     
-     // test addOperator ( + )
+    // test addOperator ( + )
     func testGivenCalculstringIsEmpty_WhenMakeOperation_ThenHaveResult(){
         calculator.numberOnScreen = "1 + 1"
         calculator.equal()
@@ -62,10 +62,10 @@ class CalculatriceTests: XCTestCase {
     }
     
     func testGivenCalculStringStartWithOperator_WhenMakingCalculation_ThenNotificationTriggered(){
-               calculator.addOperations("*")
-               XCTAssertEqual(calculator.numberOnScreen, "")
-           
-       }
+        calculator.addOperations("*")
+        XCTAssertEqual(calculator.numberOnScreen, "")
+        
+    }
     // test addOperator ( - )
     func testGivenCalculstringIsEmpty_WhenMakeMinusOperation_ThenHaveResult(){
         calculator.addNumber("1")
@@ -76,7 +76,7 @@ class CalculatriceTests: XCTestCase {
     }
     
     
-  
+    
     
     func testGivenCalculIsEmpty_WhenMakingVariousOperation_ThenResultIsGood(){
         calculator.addNumber("6")
@@ -87,24 +87,40 @@ class CalculatriceTests: XCTestCase {
         calculator.addNumber("5")
         calculator.equal()
         XCTAssertTrue(calculator.numberOnScreen == "10 / 5 = 2")
-          }
+    }
     
     func testGivenCalculHasBeenMade_WhenResultAndAddNumber_ThenResultIsNewNumber(){
-    
-                calculator.numberOnScreen = "6 + 4 = 10"
-                calculator.addNumber("2")
-          
-                
+        calculator.numberOnScreen = "6 + 4 = 10"
+        calculator.addNumber("2")
         XCTAssertEqual(calculator.numberOnScreen, "2")
     }
     
     func testGivenaddNumber_whenTappedDotNumber_ThenreturnNumber () {
-           calculator.addNumber(".")
-           XCTAssert(calculator.numberOnScreen == "0.")
-       }
-
+        calculator.addNumber(".")
+        XCTAssert(calculator.numberOnScreen == ".")
+    }
+    
+    func testGivenNumberOnScreen_WhenDoOperations_ThenReceiveNotification() {
+        expectation(forNotification: NSNotification.Name("error"), object: nil, handler: nil)
+        calculator.numberOnScreen = "2.3"
+        calculator.addOperations("b")
+        calculator.equal()
+        waitForExpectations(timeout: 0.1,  handler: nil)
+    }
+    
+    func testGivenNumberWithoutDecimal_whenAddingDot_ThenAddDot() {
+        calculator.numberOnScreen = "2"
+        calculator.addNumber(".")
+        XCTAssertEqual(calculator.numberOnScreen, "2.")
+    }
+    
+    func testGivenNumberWithDecimal_WhenAddingDot_ThenReceiveNotification() {
+        expectation(forNotification: NSNotification.Name("error"), object: nil, handler: nil)
+        calculator.numberOnScreen = "2.3"
+        calculator.addNumber (".")
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
     func testGivenCalculStringIsEmpty_WhenMakeDivisionByZero_ThenHaveAnError() {
-        
         calculator.numberOnScreen = "2 / 0"
         expectation(forNotification: NSNotification.Name(rawValue: "error"), object: nil, handler: nil)
         calculator.equal()
@@ -128,14 +144,13 @@ class CalculatriceTests: XCTestCase {
     
     
     func testGivenCalculstringIsEmpty_WhenMakeMultiplicationOperation_ThenHaveResult(){
-           
-           calculator.addNumber("2")
-           calculator.numberOnScreen.append(" x ")
-           calculator.addNumber("3")
-           calculator.equal()
-           
-           XCTAssertEqual(calculator.numberOnScreen, "2 x 3 = 6")
-       }
+        
+        calculator.addNumber("2")
+        calculator.addOperations("*")
+        calculator.addNumber("3")
+        calculator.equal()
+        XCTAssertEqual(calculator.numberOnScreen, "2 x 3 = 6")
+    }
     
     func testGivenCalculstringIsEmpty_WhenMakeDivisionOperation_ThenHaveResult(){
         
@@ -150,7 +165,7 @@ class CalculatriceTests: XCTestCase {
     
     
     func testGivenCalculstringIsEmpty_WhenMakeDAdditionAndMultiplicationOperation_ThenHaveResultInGoodOrder(){
-
+        
         calculator.addNumber("6")
         calculator.addOperations("+")
         calculator.addNumber("3")
@@ -176,19 +191,19 @@ class CalculatriceTests: XCTestCase {
     }
     
     func testGivenCalculstringIsEmpty_WhenMakeDAdditionAndMultiplicationAndDivisionOperation_ThenHaveResultInGoodOrder(){
-           
-           calculator.addNumber("6")
-           calculator.addOperations("+")
-           calculator.addNumber("4")
-           calculator.addOperations("*")
-           calculator.addNumber("10")
-           calculator.addOperations("/")
-           calculator.addNumber("2")
-           calculator.equal()
-           
-           XCTAssertEqual(calculator.numberOnScreen, "6 + 4 x 10 / 2 = 26")
-           
-       }
+        
+        calculator.addNumber("6")
+        calculator.addOperations("+")
+        calculator.addNumber("4")
+        calculator.addOperations("*")
+        calculator.addNumber("10")
+        calculator.addOperations("/")
+        calculator.addNumber("2")
+        calculator.equal()
+        
+        XCTAssertEqual(calculator.numberOnScreen, "6 + 4 x 10 / 2 = 26")
+        
+    }
     
     
 }
